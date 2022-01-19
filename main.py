5f0eaf7f086fd510937d79d50670db4e3580a6dc
NathanL21 <90645133+NathanL21@users.noreply.github.com>
2022-01-19 18:16:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from movie import movie
+
+movielist = [movie("Action", 130, 1,"loofa", "R", ["The rock", "Adam Sandler", "Chris Rock"], 2009, False) movie("comedy", 90, 3, "food", "G", ["Arnold Swarchaneggar", "Jeff Bezos", "Adam Sandler"], 2022, False) movie("SPY",144,1,"Casino Royale","R","Daniel Craig",2006, False)]
+
+Mymovie = movie("SPY","144","1","Casino Royale","93%","Daniel Craig","2006","No")
---
+++ movie.py
@@ -0,0 +1,11 @@
+class movie:
+  def __init__(self,genre, legth, part, name, rating, cast, year, animated, ):
+    self.genre = genre
+    self.legth = legth
+    self.part = part
+    self.name = name
+    self.rating = rating
+    self.cast = cast
+    self.year = year
+    self.animated = animated
+    
